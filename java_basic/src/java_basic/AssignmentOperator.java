3083a51879337017277877b2fda32f0ffb5ecc8c
sesamemanju36@gmail.com <sesamemanju36@gmail.com>
2023-11-21 15:10:59 +0900

    + 할당 연산자

---
+++ java_basic/src/java_basic/AssignmentOperator.java
@@ -0,0 +1,18 @@
+package java_basic;
+
+public class AssignmentOperator {
+
+	public static void main(String[] args) {
+		int a=12;
+		
+		int b=10;
+		
+		a += 10;
+		b -= 20;
+		
+		System.out.println(a); // a = a + 10;
+		System.out.println(b); // b = b - 20;
+		
+	}
+
+}
