63163406b0bc9be82aa52e4f9a34557daf5f6d97
sesamemanju36@gmail.com <sesamemanju36@gmail.com>
2023-11-22 17:53:33 +0900

    print

--- java_basic/src/java_basic/Print.java
+++ java_basic/src/java_basic/Print.java
@@ -1,10 +1,22 @@
 package java_basic;
 
+import java.util.Scanner;
+
 public class Print {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// 입력한 정수만큼 '*'를 찍는 프로그램
+		
+		int inputnum = 0;
+		Scanner scann = new Scanner(System.in);
+		System.out.println("임의의 정수를 입력해 주세요");
+		inputnum = scann.nextInt();
+		
+		String result="";
+		for(int i=0;i<inputnum;i++) {
+			result=result+"*";
+		}
+		System.out.println(result);
 	}
 
 }
